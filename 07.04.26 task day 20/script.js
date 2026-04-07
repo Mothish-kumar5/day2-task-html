8f081c506e4bbdb752c6b4e340181dc5b621214b
Mothish kumar V <mothishkumar2005@gmail.com>
2026-04-07 20:45:37 +0530

    Add files via upload

---
+++ 07.04.26 task day 20/script.js
@@ -0,0 +1,24 @@
+// Array
+
+let arr = [10, 20, 30];
+
+console.log("Original array:", arr);
+
+// Push array
+
+arr.push(40);
+console.log("After push:", arr);
+
+// Pop 
+arr.pop();
+console.log("After Pop:",arr);
+
+//Unshift 
+
+arr.unshift(5);
+console.log("After unshift:",arr);
+
+//Shift
+
+arr.shift();
+console.log("After shift:",arr);
